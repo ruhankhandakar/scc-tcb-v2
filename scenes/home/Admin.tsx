27e0714ee697c6c98b82bea7ec9f6d5c6bfada7e
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-10-18 21:07:18 +0530

    feat: admin ui

--- scenes/home/Admin.tsx
+++ scenes/home/Admin.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import Stats from 'components/home/Stats';
+
+import styles from './styles';
 
 const Admin = () => {
   return (
-    <View>
-      <Text>Admin</Text>
+    <View style={styles.parentContainer}>
+      <Stats styles={styles} />
     </View>
   );
 };
