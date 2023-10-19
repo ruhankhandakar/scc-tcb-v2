5ed9ec799cabdecefb29996bb3e258463d1231bf
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-10-19 20:30:45 +0530

    remove images

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,5 +1,4 @@
-export const logoIcon = require('assets/icons/logo.png') as string;
-export const searchIcon = require('assets/icons/search.png') as string;
+export const logoIcon = require('assets/app-assets/playstore.png') as string;
 export const placeholderUser =
   require('assets/images/placeholder_user.png') as string;
 export const illustration1 =
